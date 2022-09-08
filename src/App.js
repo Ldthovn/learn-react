d0cf4b6eb34a1132ce63e4ffec2b793cf296b729
VietIS Bach <bach.ngongoc@vietis.com.vn>
2022-09-08 16:19:15 +0700

    ádsd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import { Route } from "react-router-dom";
+import { Link, NavLink, Route } from "react-router-dom";
 import "./App.css";
 import TodoFeature from "./Todo";
+import Home from "./Todo/pages/Home/home";
 
 function App() {
   return (
     <div className="">
-      Home Page
-      <Route path="/todos" component={TodoFeature} />
+      <p>
+        <NavLink to="/home"> Home Page</NavLink>
+      </p>
+      <p>
+        <NavLink to="/todos">Todos</NavLink>
+      </p>
+      <Route exact path="/todos" component={TodoFeature} />
+      <Route exact path="/home" component={Home} />
     </div>
   );
 }
