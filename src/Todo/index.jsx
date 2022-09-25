2e5073ff02c8f7fdae9abf0912a28381ec8265fc
Ldthovn <ledinhtho98bn@gmail.com>
2022-09-26 06:01:10 +0700

    axios

--- src/Todo/index.jsx
+++ src/Todo/index.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import TodoList from "./TodoList";
-import { useLocation } from "react-router-dom";
-import queryString from "query-string";
 
 const initTodoList = [
   { id: 1, title: "Eat", status: "new" },
@@ -10,14 +8,8 @@ const initTodoList = [
 ];
 
 const Todo = () => {
-  const location = useLocation();
-  console.log('location', location)
   const [todoList, setTodoList] = useState(initTodoList);
-  const [filteredStatus, setFilteredStatus] = useState(() => {
-    const params = queryString.parse(location.search);
-    console.log(params)
-    return "all";
-  });
+  const [filteredStatus, setFilteredStatus] = useState("all");
 
   const handleTodoClick = (todo, index) => {
     console.log(todo, index);
@@ -46,9 +38,13 @@ const Todo = () => {
     setFilteredStatus("new");
   };
 
+  const renderedTodoList = todoList.filter(
+    (item) => filteredStatus === "all" || filteredStatus === item.status
+  );
+
   return (
     <>
-      <TodoList todoList={filteredStatus} onTodoClick={handleTodoClick} />
+      <TodoList todoList={renderedTodoList} onTodoClick={handleTodoClick} />
       <button onClick={handleShowAllClick}>All</button>
       <button onClick={handleShowCompletedClick}>Completed</button>
       <button onClick={handleShowNewClick}>New</button>
