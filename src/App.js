ea1b38612e493c3ffc4d791013c0e33758cecd23
Ldthovn <ledinhtho98bn@gmail.com>
2022-10-10 07:30:47 +0700

    Bai 68 axios api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
+import productApi from "./api/productApi";
 import "./App.css";
 import Todo from "./Todo";
 
 function App() {
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const params = {
+        _limit: 10,
+      };
+      const productList = await productApi.getAll(params);
+      console.log(productList);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div className="">
       <Todo />
